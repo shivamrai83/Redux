a5697a134200c20cfd17cf6978f7eb3bdc7ed8d3
shivamrai83 <shivamrai83@gmail.com>
2020-12-11 18:17:34 +0530

    redux Hooks added

--- my-reduxapp/src/components/cakeshop.jsx
+++ my-reduxapp/src/components/cakeshop.jsx
@@ -1,35 +1,39 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
 import {buyCake,buyIcecream} from '../redux_code'
 
-function Cakeshop(props) {
+export default function Cakeshop(props) {
+    const cakes=useSelector((state)=>state.cake.noOfCakes);
+    const icecream=useSelector((state)=>state.icecream.noOfIcecream);
+    const dispatch=useDispatch();
     return (
         <div>
-            <h2>Total Cakes-{props.noOfCakes}</h2>
-            <button onClick={props.buyCake}>Buy Cake</button>
-            <h2>sTotal IceCreams-{props.noOfIcecream}</h2>
-            <button onClick={props.buyIcecream}>Buy Cake</button>
+            <h2>Total Cakes {cakes}</h2>
+            <button onClick={()=>dispatch(buyCake())}>Buy Cake</button>
+            <h2>Total IceCreams {icecream}</h2>
+            <button onClick={()=>{dispatch(buyIcecream())}}>Buy Cake</button>
         </div>
     )
 }
 
 //selectors in seperate folder
-const mapStateToProps = (state) =>{
-    return {
-        noOfCakes:state.cake.noOfCakes,
-        noOfIcecream:state.icecream.noOfIcecream
-    }
-} 
-
-const mapDispatchToProps = (dispatch) =>{
-    return {
-        buyCake:()=>dispatch(buyCake()),
-        buyIcecream:()=>dispatch(buyIcecream())
+// const mapStateToProps = (state) =>{
+//     return {
+//         noOfCakes:state.cake.noOfCakes,
+//         noOfIcecream:state.icecream.noOfIcecream
+//     }
+// } 
+// //dispatcher
+// const mapDispatchToProps = (dispatch) =>{
+//     return {
+//         buyCake:()=>dispatch(buyCake()),
+//         buyIcecream:()=>dispatch(buyIcecream())
        
-    }
-}
+//     }
+// }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-    )(Cakeshop)
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps,
+//     )(Cakeshop)
--- my-reduxapp/src/redux_code/cake/store.js
+++ my-reduxapp/src/redux_code/cake/store.js
@@ -1,12 +1,12 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {CakeReducer, IcecreamReducer} from './cakeReducers'
-
+import logger from 'react-logger'
 import {combineReducers} from 'redux'
 
 const rootReducer=combineReducers({
 cake:CakeReducer,
 icecream:IcecreamReducer
 })
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(logger));
 console.log(store.getState())
 export default store;
